507727a751eefad65e40fbe41d46531da3a09f65
Maria Knyazeva <m.knyazeva@mail.ru>
2023-06-22 22:32:52 +0400

    Program.cs added for Task3-3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число N >> ");
+string inputstring = Console.ReadLine();
+int Nnumb = Convert.ToInt32(inputstring);
+List<string> Cube = new List<string>();
+for (int numb = 1; numb <= Nnumb; numb++)
+{
+    int cubenumbres = numb * numb * numb;
+    string str = Convert.ToString(cubenumbres);
+    Cube.Add(str);    
+}
+string strcube = String.Join(',', Cube);
+Console.WriteLine($" -> {strcube}");
